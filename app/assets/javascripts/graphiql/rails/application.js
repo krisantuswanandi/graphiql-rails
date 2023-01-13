0bd2fbbd2d96e019cdfcdd4db373576613182914
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-01-13 12:15:12 +0700

    update deps

--- app/assets/javascripts/graphiql/rails/application.js
+++ app/assets/javascripts/graphiql/rails/application.js
@@ -1,5 +1,5 @@
-//= require ./react-16.14.0
-//= require ./react-dom-16.14.0
+//= require ./react-18.2.0
+//= require ./react-dom-18.2.0
 //= require ./fetch-0.10.1
-//= require ./graphiql-1.4.2
+//= require ./graphiql-2.2.0
 //= require ./graphiql_show
